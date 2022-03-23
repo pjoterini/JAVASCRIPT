8bb7b430591b9f4458fe59a13eeaf83d022bed06
pjoterini <piotrekjan.gorski@gmail.com>
2022-03-23 19:33:37 +0100

    js

---
+++ TraversyMedia/JScrashCourse/script.js
@@ -0,0 +1,13 @@
+//   Prymitywne typy danych - stringi, liczby, booleany, null, undefined, symbol
+
+let name = 'John';
+const age = 30;
+const isCool = true;
+const x = null;
+
+const y = undefined;
+let undefinedd;
+
+// Konkatenacja
+
+console.log('my name is ' + name + ' and i am ' + age)
