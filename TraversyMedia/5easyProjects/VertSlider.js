aa07fd0650171f5702239d7df18b822c92542b8e
pjoterini <piotrekjan.gorski@gmail.com>
2022-03-31 12:50:33 +0200

    practicin

--- TraversyMedia/5easyProjects/VertSlider.js
+++ TraversyMedia/5easyProjects/VertSlider.js
@@ -1,8 +1,27 @@
 const sliderContainer = document.querySelector('.slider-container')
 const slideRight = document.querySelector('.right-slide')
 const slideLeft = document.querySelector('.left-slide')
-const upButtom = document.querySelector('.up-button')
+const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
 const slidesLength = slideRight.querySelectorAll('div').length
 
 let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+   const sliderHeight = sliderContainer.clientHeight
+   if(direction === 'up' || 'down') {
+       activeSlideIndex++
+       if (activeSlideIndex > slidesLength - 1) {
+           activeSlideIndex = 0
+       }
+   }
+
+   slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+   slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+
+}
