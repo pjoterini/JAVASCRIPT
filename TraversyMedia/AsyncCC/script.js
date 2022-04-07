9923f697a4e768d78c959c43d48943e2fb7c17d9
pjoterini <piotrekjan.gorski@gmail.com>
2022-04-07 18:25:02 +0200

    uf

--- TraversyMedia/AsyncCC/script.js
+++ TraversyMedia/AsyncCC/script.js
@@ -1,87 +1,71 @@
-const posts = [ 
-    { title: 'post one', body: 'tis is post one'},
-    { title: 'post two', body: 'tis is post two'},
-]
-
 // CALLBACKS
+//  const posts = [
+//      { title: 'post one', body: 'this is post one'},
+//      { title: 'post two', body: 'this is post two'}
+//  ]
 
-// function getPost() {
-//     setTimeout(() => {
-// let output = '';
-    
-//     posts.forEach((post) => {
-//     output += `<li>${post.title}</li>`
-//     })
-//     document.body.innerHTML = output
-//     }, 1000)
-// } 
+//  function getPosts() {
+//      setTimeout(() => {
+//         let output = ''
+//         posts.forEach((post, index) =>{
+//         output += `<li>${post.title}</li>`
+        
+//         document.body.innerHTML = output
+//         })
+//      }, 1000) 
+//  }
 
-// function createPost(post, callback) {
+//  function createPost(post, callback) {
 //     setTimeout(() => {
-//         posts.push(post)
-//         callback()
+//     posts.push(post)
+//     callback()
 //     }, 2000)
-// }
+//  }
 
-// createPost(
-//     {title: 'post three', body: 'this is post three'},
-//     getPost
-// )
+//  createPost({ title: 'post three', body: 'this is post three'}, getPosts)
 
 // PROMISES
 
-// function getPost() {
-//     setTimeout(() => {
-// let output = '';
-    
-//     posts.forEach((post) => {
-//     output += `<li>${post.title}</li>`
-//     })
-//     document.body.innerHTML = output
-//     }, 1000)
-// } 
+ const posts = [
+    { title: 'post one', body: 'this is post one'},
+    { title: 'post two', body: 'this is post two'}
+]
 
-// function createPost(post) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             posts.push(post)
-           
-//             const error = false
+function getPosts() {
+    setTimeout(() => {
+       let output = ''
+       posts.forEach((post, index) =>{
+       output += `<li>${post.title}</li>`
+       
+       document.body.innerHTML = output
+       })
+    }, 1000) 
+}
 
-//             if(!error) {
-//                 resolve()
-//            } else {
-//                reject('error: smth went wrong')
-//            }
-//         }, 2000)
-//     })
+function createPost(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        posts.push(post)
+
+        const error = false
+
+        if(!error){
+            resolve()
+        } else {
+            reject('error, smth went wrong')
+        }
+        },2000)
+    })
    
-// }
+}
 
-// createPost({title: 'post three', body: 'this is post three'})
-// .then(getPost)
+// createPost({ title: 'post three', body: 'this is post three'})
+// .then(getPosts)
 // .catch(err => console.log(err))
 
 // PROMISE.ALL
+const promise1 = Promise.resolve('hello') 
+const promise2 = 10 
+const promise3 = new Promise((resolve, reject) =>  setTimeout(resolve, 2000,'goodbye'))
 
-// const promise1 = Promise.resolve('hello boi')
-// const promise2 = 10
-// const promise3 = new Promise((resolve, reject) => setTimeout(
-//     resolve, 2000, 'goodbye'
-// ))
-
-// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res =>
-// res.json())
-
-// Promise.all([promise1, promise2, promise3, promise4]).then((
-//     (values) => console.log(values)));
-
-// ASYNC AWAIT
-
-// function init() {
-//     await createPost ({title: 'post three', body: 'this is post three'})
-
-//     getPost()
-// }
-
-// init()
+Promise.all([promise1, promise2, promise3]).then(values => console.log(values))
