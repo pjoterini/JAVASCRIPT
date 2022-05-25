67e30935b38e9847ca7c0b1aab19000aced195c8
pjoterini <piotrekjan.gorski@gmail.com>
2022-05-25 20:10:21 +0200

    fkin apis

---
+++ APIs/module2.js
@@ -0,0 +1,10 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+app.listen(3000, () => console.log('listening at 3000'))
+app.use(express.static('public'))
+
+
+app.post('/api', (request, response) => {
+    console.log(request)
+})
